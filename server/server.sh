c0962d15e61cd7ff9b488f3695b80e64a70cfeb4
Pascal MIETLICKI <p.mietlicki@gmail.com>
2023-09-28 14:40:13 +0200

    Create server.sh

---
+++ server/server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Set the port
+PORT=5000
+
+# Stop any program currently running on the set port
+echo 'preparing port' $PORT '...'
+fuser -k 5000/tcp
+
+# Switch directory
+cd build/web/
+
+# Start the server
+echo 'Server starting on port' $PORT '...'
+python3 -m http.server $PORT
